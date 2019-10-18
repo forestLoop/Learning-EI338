1731e54449658ac1fbd9b3859c6b339621b13812
keithnull <keith1126@126.com>
2019-10-18 19:39:38 +0800

    Reindent and reformat code

--- Project-1/jiffies_module.c
+++ Project-1/jiffies_module.c
@@ -19,35 +19,37 @@ static struct file_operations proc_ops = {
 };
 
 /* This function is called when the module is loaded. */
-static int proc_init(void) {
-  // creates the /proc/jiffies entry
-  proc_create(PROC_NAME, 0, NULL, &proc_ops);
-  printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
-  return 0;
+static int proc_init(void)
+{
+    // creates the /proc/jiffies entry
+    proc_create(PROC_NAME, 0, NULL, &proc_ops);
+    printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+    return 0;
 }
 
 /* This function is called when the module is removed. */
-static void proc_exit(void) {
-  // removes the /proc/jiffies entry
-  remove_proc_entry(PROC_NAME, NULL);
-  printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
+static void proc_exit(void)
+{
+    // removes the /proc/jiffies entry
+    remove_proc_entry(PROC_NAME, NULL);
+    printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
 }
 
 /*This function is called each time the /proc/jiffies is read. */
-static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
-  char buffer[BUFFER_SIZE];
-  int rv = 0;
-  static int completed = 0;
-
-  if (completed) {
-    completed = 0;
-    return 0;
-  }
-  completed = 1;
-  rv = sprintf(buffer, "Current value of jiffies: %lu\n", jiffies);
-  // copies the contents of buffer to userspace usr_buf
-  raw_copy_to_user(usr_buf, buffer, rv);
-  return rv;
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+    char buffer[BUFFER_SIZE];
+    int rv = 0;
+    static int completed = 0;
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+    completed = 1;
+    rv = sprintf(buffer, "Current value of jiffies: %lu\n", jiffies);
+    // copies the contents of buffer to userspace usr_buf
+    raw_copy_to_user(usr_buf, buffer, rv);
+    return rv;
 }
 
 /* Macros for registering module entry and exit points. */
--- Project-1/seconds_module.c
+++ Project-1/seconds_module.c
@@ -22,36 +22,38 @@ static struct file_operations proc_ops = {
 };
 
 /* This function is called when the module is loaded. */
-static int proc_init(void) {
-  // creates the /proc/seconds entry
-  proc_create(PROC_NAME, 0, NULL, &proc_ops);
-  printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
-  start_jiffies = jiffies;
-  return 0;
+static int proc_init(void)
+{
+    // creates the /proc/seconds entry
+    proc_create(PROC_NAME, 0, NULL, &proc_ops);
+    printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+    start_jiffies = jiffies;
+    return 0;
 }
 
 /* This function is called when the module is removed. */
-static void proc_exit(void) {
-  // removes the /proc/seconds entry
-  remove_proc_entry(PROC_NAME, NULL);
-  printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
+static void proc_exit(void)
+{
+    // removes the /proc/seconds entry
+    remove_proc_entry(PROC_NAME, NULL);
+    printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
 }
 
 /*This function is called each time the /proc/seconds is read. */
-static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
-  char buffer[BUFFER_SIZE];
-  int rv = 0;
-  static int completed = 0;
-
-  if (completed) {
-    completed = 0;
-    return 0;
-  }
-  completed = 1;
-  rv = sprintf(buffer, "Elapsed seconds: %lus\n", (jiffies - start_jiffies)/HZ);
-  // copies the contents of buffer to userspace usr_buf
-  raw_copy_to_user(usr_buf, buffer, rv);
-  return rv;
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+    char buffer[BUFFER_SIZE];
+    int rv = 0;
+    static int completed = 0;
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+    completed = 1;
+    rv = sprintf(buffer, "Elapsed seconds: %lus\n", (jiffies - start_jiffies) / HZ);
+    // copies the contents of buffer to userspace usr_buf
+    raw_copy_to_user(usr_buf, buffer, rv);
+    return rv;
 }
 
 /* Macros for registering module entry and exit points. */
