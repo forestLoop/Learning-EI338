6b1a72f579cca40269c3106a67e9fe0a22faf1ec
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-03-30 21:53:08 +0800

    Update hello.c
    Make it run in Ubuntu 22.04.2 LTS

--- ch2_sourec_code/hello.c
+++ ch2_sourec_code/hello.c
@@ -10,6 +10,7 @@
  * Copyright John Wiley & Sons - 2018
  */
 
+#include <linux/version.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -24,13 +25,50 @@
 /**
  * Function prototypes
  */
-ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 
+// #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+// LINUX_VERSION_CODE return the Linux version 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
+#define HAVE_PROC_OPS
+#endif
+
+#ifdef HAVE_PROC_OPS
+static struct proc_ops proc_ops = {
+        .proc_read = proc_read
+        /*
+        static const struct proc_ops proc_statsdelta_ops = {
+	    .proc_read	= proc_read,
+	    .proc_open	= proc_statsdelta_open,
+	    .proc_release	= proc_close,
+	    .proc_lseek	= default_llseek,
+        };
+
+        
+        struct proc_ops {
+        	unsigned int proc_flags;
+        	int	(*proc_open)(struct inode *, struct file *);
+        	ssize_t	(*proc_read)(struct file *, char __user *, size_t, loff_t *);
+        	ssize_t (*proc_read_iter)(struct kiocb *, struct iov_iter *);
+        	ssize_t	(*proc_write)(struct file *, const char __user *, size_t, loff_t *);
+        	loff_t	(*proc_lseek)(struct file *, loff_t, int);
+        	int	(*proc_release)(struct inode *, struct file *);
+        	__poll_t (*proc_poll)(struct file *, struct poll_table_struct *);
+        	long	(*proc_ioctl)(struct file *, unsigned int, unsigned long);
+        #ifdef CONFIG_COMPAT
+        	long	(*proc_compat_ioctl)(struct file *, unsigned int, unsigned long);
+        #endif
+        	int	(*proc_mmap)(struct file *, struct vm_area_struct *);
+        	unsigned long (*proc_get_unmapped_area)(struct file *, unsigned long, unsigned long, unsigned long, unsigned long);
+        } __randomize_layout;
+        */
+};
+#else
 static struct file_operations proc_ops = {
         .owner = THIS_MODULE,
         .read = proc_read,
 };
-
+#endif
 
 /* This function is called when the module is loaded. */
 int proc_init(void)
@@ -70,7 +108,7 @@ void proc_exit(void) {
  * count:
  * pos:
  */
-ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
         int rv = 0;
         char buffer[BUFFER_SIZE];
