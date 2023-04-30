ab2ef18da47c100357bb54b1315ec8ee6bf8c053
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-04-30 15:52:47 +0800

    updates

--- Project-4/driver.c
+++ Project-4/driver.c
@@ -18,15 +18,32 @@
 
 int main(int argc, char *argv[]) {
     FILE *in;
+    // Each FILE object denotes a C stream.
     char *temp;
     char task[SIZE];
     char *name;
     int priority;
     int burst;
     in = fopen(argv[1], "r");
+    // std::FILE* fopen( const char* filename, const char* mode );
+    // filename	-	file name to associate the file stream to
+    // mode	-	null-terminated character string determining file access mode
+    // "r"	read
+    // If successful, returns a pointer to the object that controls 
+    // the opened file stream, with both eof and error bits cleared.
     while (fgets(task, SIZE, in) != NULL) {
+        // char *fgets( char *restrict str, int count, FILE *restrict stream );
+        // str	-	pointer to an element of a char array
+        // count	-	maximum number of characters to write
+        // stream	-	file stream to read the data from
+        // retuen value : str on success, null pointer on failure.
         temp = strdup(task);
+        // char *strdup( const char *src );
+        // Returns a pointer to a null-terminated byte string, 
+        // which is a duplicate of the string pointed to by src. 
+        // The returned pointer must be passed to free to avoid a memory leak.
         name = strsep(&temp, ",");
+        // char *strsep(char **restrict stringp, const char *restrict delim);
         priority = atoi(strsep(&temp, ","));
         burst = atoi(strsep(&temp, ","));
         // add the task to the scheduler's list of tasks
